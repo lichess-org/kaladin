7a349deb5d9c3e65a6943c3ca1f793ba0f292854
kraktus <kraktus@users.noreply.github.com>
2022-01-03 23:30:59 +0100

    simplify queue test

--- src/tests/no_fork_queue_test.py
+++ src/tests/no_fork_queue_test.py
@@ -15,20 +15,16 @@ if __name__ == '__main__':
         minimal_insert_many = [{'_id': x, 'priority': 100} for x in ids]
         print(minimal_insert_many)
         qm.kaladin_queue_coll.insert_many(minimal_insert_many)
-        finished = False
-        while not finished:
-            for _id in ids:
-                try:
-                    resp = qm.kaladin_queue_coll.find_one({'_id': _id})
-                    activation = resp['response']['pred']['activation']
-                except KeyError:
-                    if not finished:
-                        time.sleep(2)
-                        break
-                    else:
-                        print(f"Test no°{i}, {_id} got no activation, resp: {resp}")
-                finished = True
-                results[_id].append(activation)
+        for _id in ids:
+            resp = None
+            while resp is None:
+                bdoc = qm.kaladin_queue_coll.find_one({'_id': _id})
+                resp = bdoc.get('response')
+                if resp is not None:
+                    activation = resp['pred']['activation']
+                    results[_id].append(activation)
+                else:
+                    time.sleep(2)
             print(results)
     for _id in ids:
         print(f"{_id}, min: {min(results[_id])}, max: {max(results[_id])}")
